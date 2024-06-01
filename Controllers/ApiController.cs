00d3d6497909a367818c9747a1639fe090004383
Y.Fujino <yusuke.fujino.10@gmail.com>
2024-06-02 00:07:45 +0900

    api/salary/month?

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using Dashboard.Models.Utility;
 using Npgsql;
+using System.Data;
 
 namespace Dashboard.Controllers;
 public class ApiController(IConfiguration conf) : BaseController(conf)
@@ -14,12 +15,25 @@ public class ApiController(IConfiguration conf) : BaseController(conf)
     // ---------------------------------------------------------------------
     // [GET] api/salary
     // ---------------------------------------------------------------------
-    [Route("api/salary")]
-    public IActionResult Saraly() {
+    [Route("api/salary/{month?}")]
+    public IActionResult Saraly(string? month) {
         using (PostgresqlWorker worker = new(base.DB_HOST, base.DB_PORT, base.DB_USER, base.DB_PASS, base.DB_NAME)) {
             string sql = string.Empty;
-            sql += "select * from public.salary ";
-            using NpgsqlDataReader reader = worker.ExecuteSqlGetData(sql);
+            Dictionary<string, dynamic> prms = [];
+            sql = """
+                select 
+                    * 
+                from 
+                    public.salary 
+                """;
+            if (month is not null) {
+                sql += """
+                where 
+                    month = @month 
+                """;
+                prms.Add("@month", new KeyValuePair<DbType, dynamic>(DbType.String, month));
+            }
+            using NpgsqlDataReader reader = worker.ExecuteSqlGetData(sql, prms);
             while (reader.Read()) {
                 _logger.Debug("month: " + reader["month"].ToString() ?? "");
                 _logger.Debug("item: " + reader["payment_item"].ToString() ?? "");
